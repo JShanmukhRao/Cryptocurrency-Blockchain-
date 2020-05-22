e7f202a8a122da9f840a9e3a9e7f44a6b196a7ef
JShanmukhRao <49684626+JShanmukhRao@users.noreply.github.com>
2020-05-22 11:52:34 +0530

    Update smcoin.py

--- smcoin.py
+++ smcoin.py
@@ -79,10 +79,10 @@ class Blockchain:
         longest_chain=None
         max_len=len(self.chain)
         for node in network:
-            response=request.get(f'https://{node}/get_chain')
+            response=requests.get(f'http://{node}/get_chain')
            
             if response.status_code==200:
-                length=response.json()['lenght']
+                length=response.json()['length']
                 chain=response.json()['chain']
                 if length > max_len and self.is_chain_valid(chain):
                     max_len=length
@@ -91,6 +91,11 @@ class Blockchain:
             self.chain=longest_chain
             return True
         return False
+    
+    def rec(self):
+        network=self.node
+        for node in network:
+            response=requests.get(f'http://{node}/replace_chain')
     
 # Mining our Blockchain
     
@@ -111,7 +116,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(sender=node_address , receiver='Shubham' , amount=1)
+    blockchain.add_transaction(sender=node_address , receiver='Yash' , amount=1)
     block=blockchain.create_block(proof, previous_hash)
     self_hash=blockchain.hash(block)
     response={'message':"SuccessFull",
@@ -123,6 +128,8 @@ def mine_block():
               'self_hash':self_hash,
               'transaction': block['transaction']
               }
+    blockchain.rec()
+           
     return jsonify(response),200
 
  
@@ -170,13 +177,13 @@ def connect_nodes():
         
     response={'message':'All nodes are connected',
               'total':list(blockchain.node)}
-    return jsonify(response),2010
+    return jsonify(response),201
 
 # Replacing the chain with longest chain
 @app.route('/replace_chain', methods=['GET'])
 
 def replace_chain():
-    is_chain_valid=blockchain.replace()
+    is_chain_valid=blockchain.replace_chain()
     if is_chain_valid:
         response={'message': 'The node has different nodes so chain was replaced by longest chain',
                   'new_chain':blockchain.chain}
